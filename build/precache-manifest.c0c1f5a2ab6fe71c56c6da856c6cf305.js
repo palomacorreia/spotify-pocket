0c248ccb52985e8f23e87a1ff13e1d20f06d3719
Paloma <palomacorreiadasilva@gmail.com>
2020-12-25 17:53:08 -0300

    Projeto spotify-pocket

---
+++ build/precache-manifest.c0c1f5a2ab6fe71c56c6da856c6cf305.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c462bf13cdd8e298896b579354a87099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb761cd2b4357413733",
+    "url": "/static/css/main.d2dc93ef.chunk.css"
+  },
+  {
+    "revision": "c6b865f5467809befbd9",
+    "url": "/static/js/2.c0468c0e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.c0468c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb761cd2b4357413733",
+    "url": "/static/js/main.42c0571e.chunk.js"
+  },
+  {
+    "revision": "f29cea98986c271ac980",
+    "url": "/static/js/runtime-main.11507720.js"
+  },
+  {
+    "revision": "60df5f786342c4271a34485e660dfa29",
+    "url": "/static/media/app-intro-1.60df5f78.jpg"
+  },
+  {
+    "revision": "ac2b9dd2bc9787d57fa5e5ea5ffc5e80",
+    "url": "/static/media/app-intro-2.ac2b9dd2.jpg"
+  },
+  {
+    "revision": "eae1132707e6db0e137a298ceac30bf7",
+    "url": "/static/media/spotify-logo-green.eae11327.svg"
+  }
+]);
